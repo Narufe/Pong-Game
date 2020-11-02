9c50f980cf49df42203190ab21990d5184f951a5
Rafe <rafe.otaya@gmail.com>
2020-11-02 12:24:32 +0200

    Moving the Ball

--- pong.py
+++ pong.py
@@ -32,6 +32,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.1
+ball.dy = -0.1
 
 # Function
 def paddle_a_up():
@@ -66,5 +68,25 @@ wn.onkeypress(paddle_b_down, "Down")
 while True:
     wn.update()
 
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if (ball.ycor() > 290):
+        ball.sety(290)
+        ball.dy *= -1
+
+    if (ball.ycor() < -290):
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if (ball.xcor() > 390):
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+    if (ball.xcor() < -390):
+        ball.goto(0, 0)
+        ball.dx *= -1
 
 
